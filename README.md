81e47e5d0cc40563ff631ea7f65c47fd0caf159f
Abhijith <hrabhijith@gmail.com>
2019-02-24 20:07:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Project: log Analysis
+
+Log Analysis is a tool which analyses the database of a frontend end application and generates a detailed report of the usage.
+
+This tool generates three reports on a specific front end application usage using the application's database and logs recorded in it periodically on basis of usage traffic on the console/command line. 
+
+## Source Code
+
+The source code is written in the "log.py" file and should be run from linux machine which has PSQL installed.
+
+According to the python version installed, the code can be run using either
+`python log.py`
+
+or using 
+`python3 log.py`
+
+
+## Database
+
+* This project uses postgresql as database tool and language for queries.
+* Before running the code, 'news' database has to be setup and data should be inserted.
+
+## Views
+
+This project needs below three views to be created in the database before start. Queries to create the same are listed below. Run all the queries before running source code and after creating the database.
+
+* For the second part of the report.
+
+`create view auth_log as select a.author as author_id, count(b.path) as view_count from articles as a left join log as b on a.slug = replace(b.path,'/article/','') group by a.author;`
+
+* For the third part of the report.
+
+`create view status_all as select SUBSTRING (TO_CHAR(time, 'YYYY-MM-DD HH24:MI'), 1, 10) as date_all, count(status) as count_all from log group by date_all;`
+
+`create view status_error as select SUBSTRING (TO_CHAR(time, 'YYYY-MM-DD HH24:MI'), 1, 10) as date_error, count(status) as count_error from log where status not like '%200 OK%' group by date_error;`
+
+`create view error_percentage as select a.date_all, b.count_error::decimal/a.count_all*100 as percent_error from status_all as a join status_error as b on a.date_all = b.date_error;`
+
---
+++ log.py
@@ -0,0 +1,102 @@
+#! /usr/bin/python -3.5.2
+
+import psycopg2
+import datetime
+
+DBNAME = 'news'
+
+now = datetime.datetime.now()
+
+
+def firstQuery():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    print("Scanning log data, please wait!\n")
+    c.execute('''select a.title, count(l.path) as num
+                from articles as a left join log as l
+                on a.slug = replace(l.path, '/article/','')
+                group by a.title
+                order by num desc
+                limit 3''')
+    result = c.fetchall()
+    db.close()
+    return result
+
+
+a = firstQuery()
+
+
+def firstQueryResult():
+    print("Here is the report on: \n")
+    print("1. Most popular three articles of all time as of %s. \n" %
+          now.strftime("%Y-%m-%d %H:%M"))
+
+    for ab in a:
+        print("--The article with the title: " + "\"" +
+              str(ab[0]) + "\"" + " has got " + str(ab[1]) + " views." + '\n')
+
+
+firstQueryResult()
+
+
+def secondQuery():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    print("Scanning log data, please wait!\n")
+    c.execute('''select a.name, b.view_count
+                from authors as a join auth_log as b
+                on a.id = b.author_id
+                order by b.view_count desc
+                limit 3''')
+    result = c.fetchall()
+    db.close()
+    return result
+
+
+b = secondQuery()
+
+
+def secondQueryResult():
+    print("Here is the report on: \n")
+    print("2. Most popular article authors of all time as of %s. \n" %
+          now.strftime("%Y-%m-%d %H:%M"))
+
+    for ab in b:
+        print("--The author: " + "\"" +
+              str(ab[0]) + "\"" + " has got " + str(ab[1]) +
+              " views for their article." + '\n')
+
+
+secondQueryResult()
+
+
+def thirdQuery():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    print("Scanning log data, please wait!\n")
+    c.execute('''select to_char(date_all::date, 'FMMonth DD, YYYY')
+                as date_all,
+                round(percent_error,2) as percent_error
+                from error_percentage
+                where percent_error > 1''')
+    result = c.fetchall()
+    db.close()
+    return result
+
+
+c = thirdQuery()
+
+
+def thirdQueryResult():
+    print("Here is the report on: \n")
+    print("3. Days which had more than 1 percent of" +
+          " requests which lead to errors as of %s. \n" %
+          now.strftime("%Y-%m-%d %H:%M"))
+
+    for ab in c:
+        print("--This day: " + "\"" +
+              str(ab[0]) + "\"" + " has observed " + str(ab[1]) + "%" +
+              " of request errors." + '\n')
+
+
+thirdQueryResult()
